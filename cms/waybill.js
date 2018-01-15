10ecc92fc3c822be2d8de4c8269c67eaaa881bdf
dhulliath <983915+dhulliath@users.noreply.github.com>
2018-01-14 19:42:42 -0700

    waybill and services tweak

--- cms/waybill.js
+++ cms/waybill.js
@@ -1,10 +1,10 @@
 {
 	description: {
 		$doc_type: 'textarea',
-		doc: 'Here is our company waybill, this gives the client the ability to fill in their own details of the shipment they plan on sending with Jazoo. \n\nPlease fill in the these sections of the waybill, this will provide Jazoo with the correct instructions for the delivery requested. \n\n* Contact Name\n* Contact Location\n* Billed To\n* Date \n* Number of Packages \n* Delivery From \n* Delivery To (City & Address Location)\n\nNote* The green parts on this waybill below will print out clearly. But when printing this out remember to scale to fit and narrow margins so it prints properly.',
+		doc: 'Here is our company waybill, this gives the client the ability to fill in their own details of the shipment they plan on sending with Jazoo. \n\nPlease fill in the these sections of the waybill, this will provide Jazoo with the correct instructions for the delivery requested. \n\n* Contact Name\n* Contact Location\n* Billed To\n* Date \n* Number of Packages \n* Delivery From \n* Delivery To (City & Address Location)',
 		$abstracted_content_hidden: true,
 		abstracted_content: {
-			marked_doc: '<p>Here is our company waybill, this gives the client the ability to fill in their own details of the shipment they plan on sending with Jazoo. </p>\n<p>Please fill in the these sections of the waybill, this will provide Jazoo with the correct instructions for the delivery requested. </p>\n<ul>\n<li>Contact Name</li>\n<li>Contact Location</li>\n<li>Billed To</li>\n<li>Date </li>\n<li>Number of Packages </li>\n<li>Delivery From </li>\n<li>Delivery To (City &amp; Address Location)</li>\n</ul>\n<p>Note* The green parts on this waybill below will print out clearly. But when printing this out remember to scale to fit and narrow margins so it prints properly.</p>\n',
+			marked_doc: '<p>Here is our company waybill, this gives the client the ability to fill in their own details of the shipment they plan on sending with Jazoo. </p>\n<p>Please fill in the these sections of the waybill, this will provide Jazoo with the correct instructions for the delivery requested. </p>\n<ul>\n<li>Contact Name</li>\n<li>Contact Location</li>\n<li>Billed To</li>\n<li>Date </li>\n<li>Number of Packages </li>\n<li>Delivery From </li>\n<li>Delivery To (City &amp; Address Location)</li>\n</ul>\n',
 			contents: []
 		}
 	},
