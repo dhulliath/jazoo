a4836cf09a4beb0a72b68c28b8ded6e53e57e0cf
dhulliath <983915+dhulliath@users.noreply.github.com>
2018-01-14 19:48:22 -0700

    rates page

--- cms/rates.js
+++ cms/rates.js
@@ -1,9 +1,9 @@
 {
 	$doc_type: 'textarea',
-	doc: 'Please contact your nearest location for our current rates.',
+	doc: 'Please contact your nearest location for our current regular route rates.\n\nOur Hot Shot services are provided at $1.85 per kilometer, only when your cargo is on board.',
 	$abstracted_content_hidden: true,
 	abstracted_content: {
-		marked_doc: '<p>Please contact your nearest location for our current rates.</p>\n',
+		marked_doc: '<p>Please contact your nearest location for our current regular route rates.</p>\n<p>Our Hot Shot services are provided at $1.85 per kilometer, only when your cargo is on board.</p>\n',
 		contents: []
 	}
 }
