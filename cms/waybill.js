17b378a448ca1b46edcafc2411cc5ee4083f8877
Peter Olejniczak <dhulliath@gmail.com>
2018-01-30 10:33:49 -0700

    merge waybill with global depots

--- cms/waybill.js
+++ cms/waybill.js
@@ -7,32 +7,5 @@
 			marked_doc: '<p>Here is our company waybill, this gives the client the ability to fill in their own details of the shipment they plan on sending with Jazoo. </p>\n<p>Please fill in the these sections of the waybill, this will provide Jazoo with the correct instructions for the delivery requested. </p>\n<ul>\n<li>Contact Name</li>\n<li>Contact Location</li>\n<li>Billed To</li>\n<li>Date </li>\n<li>Number of Packages </li>\n<li>Delivery From </li>\n<li>Delivery To (City &amp; Address Location)</li>\n</ul>\n',
 			contents: []
 		}
-	},
-	$dispatch_type_hidden: true,
-	dispatch_type: {
-		city: {
-			name: '',
-			phone: ''
-		}
-	},
-	$dispatches_templatitator: '@@dispatch_type',
-	$dispatches_expander: true,
-	dispatches: [
-		{
-			name: 'Calgary',
-			phone: '403-660-5504'
-		},
-		{
-			name: 'Edmonton',
-			phone: '780-903-3628'
-		},
-		{
-			name: 'Fort McMurray',
-			phone: '587-645-6364'
-		},
-		{
-			name: 'Grande Prairie',
-			phone: '587-297-8406'
-		}
-	]
+	}
 }
